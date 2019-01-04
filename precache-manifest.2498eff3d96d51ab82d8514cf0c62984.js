4fbda4487b18c6d4c7c55c51b2d714026c665770
daphay <dafizo1@gmail.com>
2019-01-04 14:59:23 +0100

    Updates

---
+++ precache-manifest.2498eff3d96d51ab82d8514cf0c62984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ffee4e2b5395cce6bd9",
+    "url": "/Contact-Manager-App-/static/js/runtime~main.7ffee4e2.js"
+  },
+  {
+    "revision": "80e2cd060a81f447b01f",
+    "url": "/Contact-Manager-App-/static/js/main.80e2cd06.chunk.js"
+  },
+  {
+    "revision": "3b8311272200f727d4e5",
+    "url": "/Contact-Manager-App-/static/js/1.3b831127.chunk.js"
+  },
+  {
+    "revision": "80e2cd060a81f447b01f",
+    "url": "/Contact-Manager-App-/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "3b8311272200f727d4e5",
+    "url": "/Contact-Manager-App-/static/css/1.73b7cec7.chunk.css"
+  },
+  {
+    "revision": "1d3a8e84795b4943d8b21f0623714918",
+    "url": "/Contact-Manager-App-/index.html"
+  }
+];
